57f188bbdadf323854267572ae93cb2be75b45f0
Stefan Weber <stefan.weber@think-modular.com>
2025-11-05 13:18:11 +0100

    Up version

--- version.php
+++ version.php
@@ -32,8 +32,8 @@ $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [];
 
 // Version.
-$plugin->version = 2025110501;
-$plugin->release = '1.0.1';
+$plugin->version = 2025110502;
+$plugin->release = '1.0.2';
 
 // Supported versions.
 $plugin->supported = [405, 500];
